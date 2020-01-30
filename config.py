2a9da2f3564e83b65207357aad3bf818d5771bb1
Susanna Huhtanen <susanna.huhtanen@iki.fi>
2020-01-30 20:40:31 +0100

    initial screens for different states of StateMachine

--- config.py
+++ config.py
@@ -50,5 +50,78 @@ dashboard = {
             'conversion': 'int',
             'max_age': 60
         }
-    }
+    },
+   'not-under-way': {
+        'environment.inside.salon.temperature': {
+            'label': 'Temp',
+            'unit': '°C',
+            'conversion': 'K',
+            'max_age': 60
+        },
+        'environment.inside.salon.pressure': {
+            'label': 'Baro',
+            'unit': 'hPa',
+            'conversion': 'Pa',
+            'max_age': 60
+        },
+        'environment.inside.salon.humidity': {
+            'label': 'Humid',
+            'unit': '%',
+            'conversion': 'int',
+            'max_age': 60
+        }
+   },
+   'anchored': {
+        'environment.depth.belowTransducer': {
+            'label': 'Depth',
+            'unit': 'm',
+            'conversion': 'm',
+            'max_age': 20
+        },
+        'navigation.anchor.currentRadius': {
+            'label': 'Current radius',
+            'unit': 'm',
+            'conversion': 'm',
+            'max_age': 10
+        },
+        'navigation.anchor.position': {
+            'label': 'Anchor Position',
+            'unit': 'm',
+            'conversion': 'm',
+            'max_age': '60'
+        },
+        'navigation.speedOverGround': {
+            'label': 'SOG',
+            'unit': 'kn',
+            'conversion': 'm/s',
+            'max_age': 30
+        },
+        'environment.inside.salon.pressure': {
+            'label': 'Baro',
+            'unit': 'hPa',
+            'conversion': 'Pa',
+            'max_age': 60
+        }
+   },
+  'sailing': {
+        'navigation.speedOverGround': {
+            'label': 'SOG',
+            'unit': 'kn',
+            'conversion': 'm/s',
+            'max_age': 30
+        },
+        'environment.wind.speedTrue': {
+            'label': 'Wind spd',
+            'unit': 'kn',
+            'conversion': 'm/s',
+            'max_age': 30
+        },
+        'environment.depth.belowTransducer': {
+            'label': 'Depth',
+            'unit': 'm',
+            'conversion': 'm',
+            'max_age': 20
+        }
+  }
+# 'under-engine';
 }
