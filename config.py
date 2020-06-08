1ab2818445f181d212df7d927ded55f010dd1130
Susanna Huhtanen <susanna.huhtanen@iki.fi>
2020-06-08 15:20:16 +0200

    Update display values

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ dashboard = {
     },
     'loading': {},
     'default': {
+        # Main, max 3
         'navigation.speedOverGround': {
             'label': 'SOG',
             'unit': 'kn',
@@ -29,12 +30,7 @@ dashboard = {
             'conversion': 'm',
             'max_age': 20
         },
-        'environment.wind.speedTrue': {
-            'label': 'Wind',
-            'unit': 'kn',
-            'conversion': 'm/s',
-            'max_age': 30
-        },
+        # Additional, max 4
         'environment.outside.temperature': {
             'label': 'Temp',
             'unit': '°C',
@@ -52,14 +48,67 @@ dashboard = {
             'unit': '%',
             'conversion': '%',
             'max_age': 60
+        },
+        'environment.inside.salon.temperature': {
+            'label': 'Inside',
+            'unit': '°C',
+            'conversion': 'K',
+            'max_age': 60
         }
     },
    'moored': {
-        'environment.wind.speedTrue': {
-            'label': 'Wind',
-            'unit': 'kn',
-            'conversion': 'm/s',
-            'max_age': 30
+        # Main, max 3
+        'environment.outside.temperature': {
+            'label': 'Temp',
+            'unit': '°C',
+            'conversion': 'K',
+            'max_age': 60
+        },
+        'environment.outside.pressure': {
+            'label': 'Baro',
+            'unit': 'hPa',
+            'conversion': 'Pa',
+            'max_age': 60
+        },
+        'environment.outside.humidity': {
+            'label': 'Humid',
+            'unit': '%',
+            'conversion': '%',
+            'max_age': 60
+        },
+        # Additional, max 4
+        'environment.inside.salon.temperature': {
+            'label': 'Inside',
+            'unit': '°C',
+            'conversion': 'K',
+            'max_age': 60
+        },
+        'environment.inside.refrigerator.temperature': {
+            'label': 'Fridge',
+            'unit': '°C',
+            'conversion': 'K',
+            'max_age': 60
+        },
+        'environment.inside.freezer.temperature': {
+            'label': 'Champagne',
+            'unit': '°C',
+            'conversion': 'K',
+            'max_age': 60
+        },
+        'environment.inside.engineRoom.temperature': {
+            'label': 'Beer',
+            'unit': '°C',
+            'conversion': 'K',
+            'max_age': 60
+        }
+   },
+   'anchored': {
+        # Main, max 3
+        'navigation.anchor.currentRadius': {
+            'label': 'Anchor',
+            'unit': 'm',
+            'conversion': 'm',
+            'max_age': 10
         },
         'environment.outside.temperature': {
             'label': 'Temp',
@@ -71,6 +120,59 @@ dashboard = {
             'label': 'Baro',
             'unit': 'hPa',
             'conversion': 'Pa',
+            'max_age': 60
+        },
+        # Additional, max 4
+        'environment.outside.humidity': {
+            'label': 'Humid',
+            'unit': '%',
+            'conversion': '%',
+            'max_age': 60
+        },
+        'environment.inside.salon.temperature': {
+            'label': 'Inside',
+            'unit': '°C',
+            'conversion': 'K',
+            'max_age': 60
+        },
+        'environment.inside.refrigerator.temperature': {
+            'label': 'Fridge',
+            'unit': '°C',
+            'conversion': 'K',
+            'max_age': 60
+        },
+        'environment.inside.freezer.temperature': {
+            'label': 'Champagne',
+            'unit': '°C',
+            'conversion': 'K',
+            'max_age': 60
+        }
+   },
+  'sailing': {
+        # Main, max 3
+        'navigation.speedOverGround': {
+            'label': 'SOG',
+            'unit': 'kn',
+            'conversion': 'm/s',
+            'max_age': 30
+        },
+        'navigation.courseOverGroundTrue': {
+            'label': 'COG',
+            'unit': '°',
+            'conversion': 'rad',
+            'max_age': 20
+        },
+        'environment.outside.pressure': {
+            'label': 'Baro',
+            'unit': 'hPa',
+            'conversion': 'Pa',
+            'max_age': 60
+        },
+        # Additional, max 4
+        'environment.outside.temperature': {
+            'label': 'Temp',
+            'unit': '°C',
+            'conversion': 'K',
             'max_age': 60
         },
         'environment.outside.humidity': {
@@ -89,94 +191,6 @@ dashboard = {
             'label': 'Fridge',
             'unit': '°C',
             'conversion': 'K',
-            'max_age': 60
-        }
-   },
-   'anchored': {
-        'navigation.anchor.currentRadius': {
-            'label': 'Anchor',
-            'unit': 'm',
-            'conversion': 'm',
-            'max_age': 10
-        },
-        'environment.depth.belowTransducer': {
-            'label': 'Depth',
-            'unit': 'm',
-            'conversion': 'm',
-            'max_age': 20
-        },
-        'environment.wind.speedTrue': {
-            'label': 'Wind',
-            'unit': 'kn',
-            'conversion': 'm/s',
-            'max_age': 30
-        },
-        'environment.outside.temperature': {
-            'label': 'Temp',
-            'unit': '°C',
-            'conversion': 'K',
-            'max_age': 60
-        },
-        'environment.outside.pressure': {
-            'label': 'Baro',
-            'unit': 'hPa',
-            'conversion': 'Pa',
-            'max_age': 60
-        },
-        'environment.outside.humidity': {
-            'label': 'Humid',
-            'unit': '%',
-            'conversion': '%',
-            'max_age': 60
-        },
-        'environment.inside.salon.temperature': {
-            'label': 'Inside',
-            'unit': '°C',
-            'conversion': 'K',
-            'max_age': 60
-        },
-   },
-  'sailing': {
-        'navigation.speedOverGround': {
-            'label': 'SOG',
-            'unit': 'kn',
-            'conversion': 'm/s',
-            'max_age': 30
-        },
-        'navigation.courseOverGroundTrue': {
-            'label': 'COG',
-            'unit': '°',
-            'conversion': 'rad',
-            'max_age': 20
-        },
-        'environment.wind.speedTrue': {
-            'label': 'Wind',
-            'unit': 'kn',
-            'conversion': 'm/s',
-            'max_age': 30
-        },
-        'environment.depth.belowTransducer': {
-            'label': 'Depth',
-            'unit': 'm',
-            'conversion': 'm',
-            'max_age': 20
-        },
-        'environment.outside.pressure': {
-            'label': 'Baro',
-            'unit': 'hPa',
-            'conversion': 'Pa',
-            'max_age': 60
-        },
-        'environment.outside.temperature': {
-            'label': 'Temp',
-            'unit': '°C',
-            'conversion': 'K',
-            'max_age': 60
-        },
-        'environment.outside.humidity': {
-            'label': 'Humid',
-            'unit': '%',
-            'conversion': '%',
             'max_age': 60
         }
   }
